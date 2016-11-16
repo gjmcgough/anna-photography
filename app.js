79ff6c74986574800de32d7b70783825c3b422f7
Greg M <applecomputer@Apples-MacBook.local>
2016-11-15 18:05:23 -0800

    add angular and render hello world.

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var app = angular.module('annaPhotography', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.image = 'Hello world!';
 }]);
+$scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+];
