3b3c741dd0bbf62cfefd5ada9c00960f7e3440e4
Greg M <applecomputer@Apples-MacBook.local>
2016-11-16 01:04:26 -0800

    move views to views folder.

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ app.controller('ImageCtrl', [
   '$scope',
   function($scope){
     $scope.myImages = [
-      {image: "Corgi.png", title: "Corgi"},
-      {image: "secret-hideaway.png", title: "Hideaway"},
-      {image: "llama.jpg", title: "Llama"},
-      {image: "mex.jpg", title: "Mex"},
-      {image: "surfing-1400X600.jpg", title: "Pittedddd"},
-      {image: "rainbow_shave_ice_800.jpg", title: "Rainbow Shaved Ice"}
+      {image: "Corgi.png", title: "San Diego"},
+      {image: "secret-hideaway.png", title: "Chicago"},
+      {image: "llama.jpg", title: "New Orleans"},
+      {image: "mex.jpg", title: "San Francisco"},
+      {image: "surfing-1400X600.jpg", title: "Los Angeles"},
+      {image: "rainbow_shave_ice_800.jpg", title: "Baja Nor"}
     ];
 
     // $scope.titles = ["Corgi", "Hideaway", "Llama", "Mexico", "Pittedddd"]
 
     $scope.getImagePath = function(imageName) {
+      console.log(imageName);
       return  "../anna-photography/img/" + imageName;
     };
 
