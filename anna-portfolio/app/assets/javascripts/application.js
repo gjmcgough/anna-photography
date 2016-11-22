838b174a175992b9a813fbc90a94848471a0dd67
Greg M <applecomputer@Apples-MacBook.local>
2016-11-22 15:32:31 -0800

    working on templates

--- anna-portfolio/app/assets/javascripts/application.js
+++ anna-portfolio/app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require angular
-//= require angular-rails-templates
-//= require angular-ui-router
+// = require angular
+// = require angular-rails-templates
+// = require angular-ui-router
 //= require_tree .
