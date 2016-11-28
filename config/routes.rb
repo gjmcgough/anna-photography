505c519643e562bb6bc38d1d3eb0a815c84fc1eb
Greg M <applecomputer@Apples-MacBook.local>
2016-11-28 10:05:55 -0800

    add rails routes and reconfig app.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'application#angular'
 
+  resources :galleries, only: [:index, :show] do
+    resources :comments, only: [:index, :show] do
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
