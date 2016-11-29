e2fcf94a55e01429d97a4142e815e915fd22f1ad
Greg M <applecomputer@Apples-MacBook.local>
2016-11-29 13:46:14 -0800

    add get galleries to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'aws-sdk'
+
 module ApplicationHelper
+
+  def get_galleries
+    bucket = s3.bucket[anna-lizano-photo]
+    data = bucket.objects
+    return data
+    # snd request for galleries to aws
+    # save and return array containing all galleries
+  end
+
 end
