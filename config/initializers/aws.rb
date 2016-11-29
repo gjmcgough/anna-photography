95f694c82c88d45fb684fc6480815ba64662325f
Greg M <applecomputer@Apples-MacBook.local>
2016-11-29 10:48:38 -0800

    git ignore app.yml

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
-require 'aws-sdk'
-
-Rails.configuration({
-  region: 'us-west-1',
-  credentials: Aws::Credentials.new('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
-})
+# require 'aws-sdk'
+#
+# Rails.configuration({
+#   region: 'us-west-1',
+#   credentials: Aws::Credentials.new('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
+# })
