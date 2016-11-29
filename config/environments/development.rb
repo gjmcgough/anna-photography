0f4803c64fe13deb3137d53bce4d1762522aff7d
Greg M <applecomputer@Apples-MacBook.local>
2016-11-29 11:01:48 -0800

    add gallery id and cover boolean.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,6 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
--- db/migrate/20161129173653_add_cover_image_to_galleries.rb
+++
@@ -1,5 +0,0 @@
-class AddCoverImageToGalleries < ActiveRecord::Migration
-  def change
-    add_column :galleries, :image, :attachment
-  end
-end
