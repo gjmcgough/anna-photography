03f35b7536b3f5a378697816adac21eba57abfa2
Greg M <applecomputer@Apples-MacBook.local>
2016-11-29 10:20:31 -0800

    add AWS

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk'
 
-Aws.config.update({
+AWS.config.update({
   region: 'us-west-1',
-  credentials: Aws::Credentials.new('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
+  credentials: AWS::Credentials.new('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
 })
