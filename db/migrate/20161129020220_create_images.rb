f7a07bb90f90c559a32fa3d5a284dc283dd608a7
Greg M <applecomputer@Apples-MacBook.local>
2016-12-02 17:37:01 -0800

    fix migration duplication.

--- db/migrate/20161129020220_create_images.rb
+++ db/migrate/20161129020220_create_images.rb
@@ -1,7 +1,6 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-      t.attachment :image
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129223942) do
+ActiveRecord::Schema.define(version: 20161202171707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,14 +23,14 @@ ActiveRecord::Schema.define(version: 20161129223942) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "gallery_id"
     t.boolean  "cover_image",        default: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
