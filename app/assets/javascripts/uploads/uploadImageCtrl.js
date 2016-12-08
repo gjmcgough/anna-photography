1cb128843e5b1f9650ce25453581d8dec02f2ef4
Greg M <applecomputer@Apples-MacBook.local>
2016-12-08 11:27:48 -0800

    cover bool radio working.

--- app/assets/javascripts/uploads/uploadImageCtrl.js
+++ app/assets/javascripts/uploads/uploadImageCtrl.js
@@ -10,6 +10,7 @@ app.controller('UploadImageCtrl', [
     $scope.gallery = gallery;
     $scope.images = galleries.images;
     $scope.src = 'https://s3-us-west-1.amazonaws.com/anna-lizano-photo/images/images/000/000/039/original/Screen_Shot_2016-11-15_at_4.03.31_PM.png';
+    $scope.coverBool = false
     console.log($scope.src);
     console.log($scope.images[0].image_file_name);
     console.log($stateParams.image_file_name);
@@ -20,7 +21,7 @@ app.controller('UploadImageCtrl', [
         method: 'POST',
         fields: {
           'image[gallery_id]': gallery.id,
-          'image[cover_image]': false,
+          'image[cover_image]': $scope.coverBool,
           'image[image]': file
         },
         file: file,
