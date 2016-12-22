f1b29f3ddaf208a05324d836e13d52afcbbaa6de
Greg M <applecomputer@Apples-MacBook.local>
2016-12-22 13:17:25 -0800

    trying to hit the right image endpoint in s3.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,10 +16,13 @@ class ImagesController < ApplicationController
     # puts "{{{{{{}}}}}}{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{#{gallery.title}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
     image = Image.new(image_params)
     # images = gallery.images
-    image.src = s3_bucket.object(image.image_file_name).public_url
+    image.src = "https:" + image.image.url
     if image.save
       @images = Image.where(gallery_id: params[:gallery_id])
-      puts "{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{#{s3_bucket.object(image.image_file_name).public_url}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
+      puts "{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{#{s3_bucket.object(image.image_file_name).presigned_url(:get)}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
+      # signer = Aws::S3::Presigner.new
+      # url = signer.presigned_url(:get_object, bucket: 'anna-lizano-photo', key: image.image_file_name)
+      puts "{{{{{{{{{{{{{{{{{{{{{#{image.src}}}}}}}}}}}}}}}}}}}}}}"
       respond_to do |format|
          format.json { render :json => @images.to_a }
       end
