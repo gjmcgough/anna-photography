a97e388865d9a5a5de04a6282af905aaf51b8d4b
Greg M <applecomputer@Apples-MacBook.local>
2017-01-02 12:37:44 -0800

    change cover and nav ttitle

--- app/assets/javascripts/teleport/teleportCtrl.js
+++ app/assets/javascripts/teleport/teleportCtrl.js
@@ -6,6 +6,6 @@ app.controller('TeleportCtrl', [
   function($scope, galleries){
     $scope.galleries = galleries.galleries;
     $scope.cover_images = galleries.cover_images;
-    $scope.home_image = galleries.cover_images[1];
+    $scope.home_image = galleries.cover_images[0];
 
 }]);
