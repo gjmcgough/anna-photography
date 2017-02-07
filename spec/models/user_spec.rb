976f06b68c636c39c6e18b0485a68a78facc7051
Greg McGough <gjmcgough@gmail.com>
2017-02-06 20:00:01 -0800

    add coveralls

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {
+    described_class.new(email: 'admin@admin.com', password: 'password')
+  }
+
+  it "has valid email and password" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid witout email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is must have valid email" do
+    expect(subject.email).to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+  end
+end
